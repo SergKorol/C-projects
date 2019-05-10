79e1d0cf9bffbe04b9860dbf367fac0f73be847e
SergKorol <korol_sergey@me.com>
2019-05-10 21:55:38 +0300

    Add files via upload

---
+++ GlobalCityManager/Controllers/CityController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GlobalCityManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalCityManager.Controllers
+{
+    public class CityController : Controller
+    {
+        private readonly worldContext _contextCity;
+
+        public CityController()
+        {
+            _contextCity = new worldContext();
+        }
+
+        public IActionResult Index()
+        {
+            IQueryable<City> cities = from city in _contextCity.City select city;
+            var listCountries = cities.ToList();
+
+            return View(listCountries);
+        }
+
+        //Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(City newCity)
+        {
+            if (ModelState.IsValid)
+            {
+                _contextCity.City.Add(newCity);
+                _contextCity.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(_contextCity.City.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(City uCity)
+        {
+            if (ModelState.IsValid)
+            {
+                _contextCity.City.Update(uCity);
+                _contextCity.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uCity);
+        }
+
+        //Delete
+        public IActionResult Delete(string id)
+        {
+
+            var dCity = _contextCity.City.Find(id);
+
+            if (dCity != null)
+            {
+                _contextCity.City.Remove(dCity);
+                _contextCity.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ GlobalCityManager/Controllers/CountryController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GlobalCityManager.Models;
+
+namespace GlobalCityManager.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly worldContext _context;
+
+        public CountryController()
+        {
+            _context = new worldContext();
+        }
+
+        public IActionResult Index()
+        {
+            IQueryable<Country> countries = from country in _context.Country select country;
+            var listCountries = countries.ToList();
+
+            return View(listCountries);
+        }
+
+
+        //Show
+        public IActionResult ShowDetail(string id)
+        {
+
+            var sCountry = _context.Country.Find(id);
+
+            if (sCountry != null)
+
+                return View("Detail", sCountry);
+            else
+                return RedirectToAction("Index");
+
+        }
+
+       //Create
+       [HttpGet]
+       public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Country newCountry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Country.Add(newCountry);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //Edit
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            return View(_context.Country.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Country uCountry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Country.Update(uCountry);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uCountry);
+        }
+        //Delete
+        public IActionResult Delete(string id)
+        {
+
+            var dCountry = _context.Country.Find(id);
+
+            if (dCountry != null)
+            {
+                _context.Country.Remove(dCountry);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
