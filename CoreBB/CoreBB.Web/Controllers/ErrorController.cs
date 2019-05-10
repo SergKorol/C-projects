838160cc05cbc31c0d743f18d16ba0c89c823b52
SergKorol <korol_sergey@me.com>
2019-05-10 22:16:03 +0300

    Add files via upload

---
+++ CoreBB/CoreBB.Web/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreBB.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            ViewData["StatusCode"] = HttpContext.Response.StatusCode;
+            ViewData["Message"] = exception.Error.Message;
+            ViewData["StackTrace"] = exception.Error.StackTrace;
+
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
