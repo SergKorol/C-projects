998ce6da1f44bc40aaf82f3b102d38279787b24a
SergKorol <korol_sergey@me.com>
2019-05-19 14:07:51 +0300

    Add files via upload

---
+++ GuessingGame/GuessingGame/GuessingGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace GuessingGame
+{
+    public class GuessingGame
+    {
+        public const int lowInt = 1;
+        public const int upperInt = 11;
+
+        public int Random { get; private set; }
+        public int Tries { get; private set; }
+
+
+        public GuessingGame()
+        {
+            Tries = 3;
+            randomIntGenerator();
+        }
+
+        public bool Game(int num)
+        {
+            Tries--;
+            if(num < lowInt || num > upperInt)
+            {
+                throw new ArgumentOutOfRangeException("num", "Enter number from 1 to 10");
+            }
+
+            if(num == Random)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void randomIntGenerator()
+        {
+            Random generator = new Random();
+            generator.Next(lowInt, upperInt);
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            GuessingGame guessingGame = new GuessingGame();
+
+            Console.WriteLine("Guess number from {0} to {1}", GuessingGame.lowInt, GuessingGame.upperInt -1);
+            bool gameOver = false;
+            do
+            {
+                Console.WriteLine("Enter number:");
+                int playerNumber = int.Parse(Console.ReadLine());
+                try
+                {
+                    if (guessingGame.Game(playerNumber))
+                    {
+                        Console.WriteLine("You are guessed");
+                        gameOver = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again");
+                    }
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine("Invalid number: " + e.Message);
+                }
+                Console.WriteLine("Remaining tries: " + guessingGame.Tries + "\n");
+
+                if (guessingGame.Tries <= 0)
+                {
+                    break;
+                }
+            } while (!gameOver);
+            if(!gameOver)
+            {
+                Console.WriteLine("Game over");
+            }
+
+        }
+    }
+}
